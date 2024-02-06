bf1e2abf6d3bec270420d85b20479fe3f6320133
Eliecer <eliecer.sanchez47@gmail.com>
2024-02-06 19:26:09 -0400

    popup and static data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import burger from '../../public/images/burger.png'
 
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-center">Home page</h1>
-      <Button variant={"outline"}>Button</Button>
+    <div className="">
+      <h1 className="text-gray-800 text-5xl font-medium">Make Your Burger</h1>
+      <p className="text-gray-800 text-xl font-normal leading-relaxed mt-3">Parallax screen. Burger ingredients and emojis moving depending on the position of the mouse pointer.</p>
+      <div className="relative flex justify-center items-center">
+        <Button className="rounded-full h-24 w-24 text-xs absolute uppercase hover:bg-blue-900 duration-500">make burger</Button>
+        <Image src={burger} alt="burger" quality={100} className="mx-auto"/>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,23 @@
 import { Phone } from "lucide-react";
 import Logo from "./icons/Logo";
 import { Button } from "./ui/button";
-import MenuBar from "./icons/MenuBar";
+import MenuBar from "./icons/MenuBar"
+
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 const Navbar = () => {
   return (
-    <div className="bg-red-600">
+    <div className="bg-red-600 flex justify-around p-2 md:justify-start md:gap-4">
       <Button
         variant={"outline"}
         size={"icon"}
@@ -14,14 +26,46 @@ const Navbar = () => {
         <Logo />
       </Button>
 
-      <Button>
-        <Phone className="mr-2 h-4 w-4"/>  
+      <Button variant={'outline'} className="rounded-2xl bg-[#FFEDED] text-[#FF4D4F]">
+        <Phone className="mr-2 h-4 w-4 text-[#FF4D4F] "/>  
         Call Me Back
       </Button>
 
-      <Button>
+      <Sheet>
+
+      <SheetTrigger asChild>
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        className="rounded-full bg-gray-200"
+        
+      >
         <MenuBar />
       </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+           
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+           
+            
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+      </Sheet>
     </div>
   );
 };
